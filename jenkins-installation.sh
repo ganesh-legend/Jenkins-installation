18f6a2beae3ac63ed18f1b2de0b92eddb9f10345
Ubuntu <ubuntu@ip-172-31-46-72.ap-south-1.compute.internal>
2024-06-26 11:53:56 +0000

    adding executable file for jenkins installation

---
+++ jenkins-installation.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+sudo wget -O /usr/share/keyrings/jenkins-keyring.asc \
+      https://pkg.jenkins.io/debian/jenkins.io-2023.key
+
+echo "deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc]" \
+	   https://pkg.jenkins.io/debian binary/ | sudo tee \
+	         /etc/apt/sources.list.d/jenkins.list > /dev/null
+
+
+sudo apt install fontconfig openjdk-17-jre -y
+
+sudo apt install openjdk-17-jdk -y
+
+
+
+sudo apt-get update -y
+
+
+
+sudo apt-get install jenkins -y
+
+sudo systemctl enable jenkins 
+
+sudo systemctl start jenkins
+
+
